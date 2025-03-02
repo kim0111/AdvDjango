34393d0c0e523208ab39cfbc019fa5b47a7a9177
Yuriy Kim <86676168+kim0111@users.noreply.github.com>
2025-03-02 22:12:12 +0500

    Celery and Redis

--- MiniProject1/MiniProject1/celery.py
+++ MiniProject1/MiniProject1/celery.py
@@ -3,6 +3,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MiniProject1.settings')
 
-celery_app = Celery('')
+celery_app = Celery('MiniProject1')
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 celery_app.autodiscover_tasks()
